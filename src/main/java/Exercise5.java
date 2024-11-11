d3fa5d2311fca8dadae26bbbd65298fd91cc67b9
aw2095 <aw2095@cam.ac.uk>
2024-11-11 15:23:27 +0000

    Implemented lowestCommon, and added test from exercise sheet.

--- src/main/java/Exercise5.java
+++ src/main/java/Exercise5.java
@@ -1,5 +1,15 @@
 public class Exercise5 {
     static int lowestCommon(long a, long b) {
-        throw new UnsupportedOperationException();
+        long setBitsInCommon = a & b;
+        if (setBitsInCommon == 0) {
+            return -1;
+        }
+        int position = 0;
+        // Find position of least significant 1 in bitsInCommon
+        while ((setBitsInCommon & 1) == 0) {
+            setBitsInCommon >>= 1;
+            position++;
+        }
+        return position;
     }
 }
--- src/test/java/Exercise5Test.java
+++ src/test/java/Exercise5Test.java
@@ -4,21 +4,26 @@ import org.junit.jupiter.api.Assertions;
 public class Exercise5Test {
     @Test
     public void returnsMinusOneWhenNoBitsInCommon() {
-        Assertions.assertEquals(Exercise5.lowestCommon(0b1010, 0b0101), -1);
+        Assertions.assertEquals(-1, Exercise5.lowestCommon(0b1010, 0b0101));
     }
 
     @Test
     public void returnsOneWhenAIs0111AndBIs1010() {
-        Assertions.assertEquals(Exercise5.lowestCommon(0b0111, 0b1010), 1);
+        Assertions.assertEquals(1, Exercise5.lowestCommon(0b0111, 0b1010));
     }
 
     @Test
     public void returnsTwoWhenAIs0100AndBIs1100() {
-        Assertions.assertEquals(Exercise5.lowestCommon(0b0100, 0b1100), 2);
+        Assertions.assertEquals(2, Exercise5.lowestCommon(0b0100, 0b1100));
     }
 
     @Test
     public void returnsZeroWhenAIs1111AndBIs0001() {
-        Assertions.assertEquals(Exercise5.lowestCommon(0b1111, 0b0001), 0);
+        Assertions.assertEquals(0, Exercise5.lowestCommon(0b1111, 0b0001));
+    }
+
+    @Test
+    public void returnsThreeWhenAIs14AndBIs25() {
+        Assertions.assertEquals(3, Exercise5.lowestCommon(14, 25));
     }
 }
